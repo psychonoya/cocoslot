9b22afe1a0d018e8d77b4ac93cd35c51e4d1c19d
Psychonoya <psychonoyaa@gmail.com>
2021-06-21 11:22:29 +0200

    Fixes and coins added

--- assets/Scripts/GameManager.ts
+++ assets/Scripts/GameManager.ts
@@ -5,17 +5,26 @@ const { ccclass, property } = _decorator;
 
 @ccclass('GameManager')
 export class GameManager extends Component {
-
+    // This is a the pointText node we have in our scene, set to null since we will pass the reference through  our engine
     @property({
         type:Label
     })
     pointsText: Label |null = null;
-
+    // This is a the bonusText node we have in our scene, set to null since we will pass the reference through  our engine
     @property({
         type:Label
     })
     bonusText: Label |null = null;
+    // This is a the bonusText node we have in our scene, set to null since we will pass the reference through  our engine
+    @property({
+        type:Label
+    })
+    coinsText: Label |null = null;
 
+
+
+
+    // Those are the reference of our SpriteLogic class so we can access the colorNumber and assign the points trough it
     @property
     ({
         type: SpriteLogic
@@ -33,58 +42,92 @@ export class GameManager extends Component {
         type: SpriteLogic
     })
     row3: SpriteLogic | null = null;
-
+    // those are the current points (0 by default) the number will grow once we start to play and score
     currentPoints: number = 0;
+    // this is the bonus points given
     @property
     bonusPoints: number = 10;
+    // This is the standard streak points given
+    @property
+    standardStreakPoints: number = 2;
 
+    @property
+    coins: number = 100;
+
+    @property
+    cost: number = 5;
+
+    // This function will be only used by the button and not in the code, this is the logic behind our points.
     checkRows()
     {
         if(this.row1 && this.row2 && this.row3) {
-            if (this.row1.colorNumber == 1 && this.row2.colorNumber == 1 && this.row3.colorNumber == 1 ||
-                this.row1.colorNumber == 2 && this.row2.colorNumber == 2 && this.row3.colorNumber == 2 ||
-                this.row1.colorNumber == 3 && this.row2.colorNumber == 3 && this.row3.colorNumber == 3 )
+            if (this.coins > 10)
             {
-                this.currentPoints =  +this.currentPoints  +   +this.bonusPoints
-                if (this.bonusText) {
-                    this.bonusText.string = "!!!BONUS + 10!!!";
+                // Reducing coins once we spin
+                this.coins =  this.coins-this.cost;
+                // if all of our sprite are the same color than we get bonus points
+                if (this.row1.colorNumber == 1 && this.row2.colorNumber == 1 && this.row3.colorNumber == 1 ||
+                    this.row1.colorNumber == 2 && this.row2.colorNumber == 2 && this.row3.colorNumber == 2 ||
+                    this.row1.colorNumber == 3 && this.row2.colorNumber == 3 && this.row3.colorNumber == 3 )
+                {
+                    this.currentPoints =  +this.currentPoints  +   +this.bonusPoints;
+                    if (this.bonusText) {
+                        //Changing the text every time we spin, if get the bonus than we show how many points we got
+                        this.bonusText.string = "!!!BONUS +"+this.bonusPoints+ "!!!";
+                        this.coins =  +this.coins  +   +this.bonusPoints;
+                    }
+                }
+                // otherwise every time we check if we have 2 rows having the same color, and if they are near we get +1
+                else if (this.row1.colorNumber == 1 && this.row2.colorNumber == 1 ||
+                    this.row1.colorNumber == 2 && this.row2.colorNumber == 2 ||
+                    this.row1.colorNumber == 3 && this.row2.colorNumber == 3 )
+                {
+                    this.currentPoints =  +this.currentPoints  +   +this.standardStreakPoints;
+                    this.coins =  +this.coins  +   +this.standardStreakPoints;
+                    if (this.bonusText) {
+                        this.bonusText.string = "+"+this.standardStreakPoints+"!";
+                    }
+                }
+                else if(this.row2.colorNumber == 1 && this.row3.colorNumber == 1 ||
+                    this.row2.colorNumber == 2 && this.row3.colorNumber == 2 ||
+                    this.row2.colorNumber == 3 && this.row3.colorNumber == 3)
+                {
+                    this.currentPoints =  +this.currentPoints  +   +this.standardStreakPoints;
+                    this.coins =  +this.coins  +   +this.standardStreakPoints;
+                    if (this.bonusText) {
+                        this.bonusText.string = "+"+this.standardStreakPoints+"!";
+                    }
+                }
+                else
+                {
+                    if (this.bonusText) {
+                        // If we didn't hit the streak or bonus than we just wish good luck to the player for his next spins
+                        this.bonusText.string = "Try again, Good luck!";
+                    }
                 }
             }
-            else if (this.row1.colorNumber == 1 && this.row2.colorNumber == 1 ||
-                this.row1.colorNumber == 2 && this.row2.colorNumber == 2 ||
-                this.row1.colorNumber == 3 && this.row2.colorNumber == 3 )
-            {
-                this.currentPoints++
-                if (this.bonusText) {
-                    this.bonusText.string = "+1!";
-                }
-            }
-            else if(this.row2.colorNumber == 1 && this.row3.colorNumber == 1 ||
-                this.row2.colorNumber == 2 && this.row3.colorNumber == 2 ||
-                this.row2.colorNumber == 3 && this.row3.colorNumber == 3)
-            {
-                this.currentPoints++
-                if (this.bonusText) {
-                    this.bonusText.string = "+1!";
-                }
-            }
-            else
-            {
-                if (this.bonusText) {
-                    this.bonusText.string = "Try again, Good luck!";
-                    this.bonusPoints = 10;
-                }
+            if (this.coinsText) {
+                // Every time we spin the current points are update, UI can also be updated per frame but since we don't have the need in this case, we are updating it only when spin
+                this.coinsText.string = "Coins: " + this.coins;
             }
 
         }
-
+        if (this.pointsText) {
+            // Every time we spin the current points are update, UI can also be updated per frame but since we don't have the need in this case, we are updating it only when spin
+            this.pointsText.string = "Score: " + this.currentPoints;
+        }
 
     }
 
-    update()
+    start()
     {
         if (this.pointsText) {
             this.pointsText.string = "Score: " + this.currentPoints;
+        }
+
+        if (this.coinsText) {
+            // Every time we spin the current coins are update
+            this.coinsText.string = "Coins: " + this.coins;
         }
     }
 }
--- assets/Scripts/SpriteLogic.ts
+++ assets/Scripts/SpriteLogic.ts
@@ -5,41 +5,41 @@ const { ccclass, property } = _decorator;
 @ccclass('SpriteLogic')
 export class SpriteLogic extends Component {
 
+    // Standard CocosCreator Sprite
     @property({
         type: Sprite
     })
     currSprite: Sprite | null = null;
 
+    //Defining different colors
     @property
     mixColor1 = new Color(255,0,0,255);
     @property
     mixColor2 = new Color(0,255,0,255);
     @property
     mixColor3 = new Color(0,0,255,255);
+    //Standard color
     @property
     defColor = new Color(0,0,0,0);
-
+    //Color number will let our function change color accordingly to a random given color number and also will give poits to the players once he has 2/3 streak of the same color
     @property
     colorNumber = 0;
 
     start () {
         if (this.currSprite)
         {
+            // OnStart we set up the color of our sprite to "Invisible" so we must press the button to start our game.
             this.currSprite.color = this.defColor;
-            this.changeColor();
         }
     }
 
-
+// This is the function that is actually deciding the color of our sprite, in base of the random given number.
     changeColor()
     {
         if (this.currSprite)
         {
             this.colorNumber = (Math.floor(Math.random() * 3) + 1);
             switch (this.colorNumber) {
-                case 0:
-                    this.currSprite.color = this.defColor;
-                    break;
                 case 1:
                     this.currSprite.color = this.mixColor1;
                     break;
